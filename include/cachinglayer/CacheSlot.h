34492540e2ebd8b2e3ab6646401b8387b6a9494d
chasingegg <chao.gao@zilliz.com>
2025-08-29 08:39:35 +0000

    update error msg format
    Signed-off-by: chasingegg <chao.gao@zilliz.com>

--- include/cachinglayer/CacheSlot.h
+++ include/cachinglayer/CacheSlot.h
@@ -275,13 +275,14 @@ class CacheSlot final : public std::enable_shared_from_this<CacheSlot<CellT>> {
             }
 
             if (!reservation_success) {
-                auto error_msg = fmt::format(
-                    "[MCL] CacheSlot failed to reserve resource for "
+                LOG_ERROR("[MCL] CacheSlot failed to reserve resource for "
                     "cells: key={}, cell_ids=[{}], total "
                     "resource_needed_for_loading={}",
-                    translator_->key(), fmt::join(loading_cids, ","), resource_needed_for_loading.ToString());
-                LOG_ERROR(error_msg);
-                ThrowInfo(ErrorCode::InsufficientResource, error_msg);
+                    translator_->key(), fmt::join(loading_cids, ","), resource_needed_for_loading.ToString()));
+                ThrowInfo(ErrorCode::InsufficientResource, "[MCL] CacheSlot failed to reserve resource for "
+                    "cells: key={}, cell_ids=[{}], total "
+                    "resource_needed_for_loading={}",
+                    translator_->key(), fmt::join(loading_cids, ","), resource_needed_for_loading.ToString()));
             }
 
             // defer release resource_needed_for_loading
