869e67635364b6482283f1a4b43ebef1657bea79
Shawn Wang <shawn.wang@zilliz.com>
2025-09-15 14:41:06 +0800

    enhance: add null OpContext ptr support
    Signed-off-by: Shawn Wang <shawn.wang@zilliz.com>

--- include/cachinglayer/Manager.h
+++ include/cachinglayer/Manager.h
@@ -47,9 +47,7 @@ class Manager {
         auto self_reserve = evictionEnabled_;
         auto cache_slot =
             std::make_shared<CacheSlot<CellT>>(std::move(translator), dlist_.get(), evictable, self_reserve);
-        // TODO: Warmup is not tracked for now
-        auto ignored_op_ctx = std::make_unique<OpContext>();
-        cache_slot->Warmup(ignored_op_ctx.get());
+        cache_slot->Warmup();
         return cache_slot;
     }
 
