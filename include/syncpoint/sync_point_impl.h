be7ff418a00955da23c2b794cdb4b033752f36c9
Spade  A <71589810+SpadeA-Tang@users.noreply.github.com>
2025-09-24 19:59:25 +0800

    feat: implement sync point (#37)
    Signed-off-by: SpadeA <tangchenjie1210@gmail.com>

---
+++ include/syncpoint/sync_point_impl.h
@@ -0,0 +1,117 @@
+// Adapted from RocksDB's SyncPoint implementation
+// (https://github.com/facebook/rocksdb/blob/main/test_util/sync_point.h)
+
+#include <assert.h>
+
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "syncpoint/sync_point.h"
+
+#pragma once
+
+#ifdef ENABLE_SYNCPOINT
+namespace milvus {
+
+struct SyncPoint::Data {
+    Data() : enabled_(false) {
+    }
+    // Enable proper deletion by subclasses
+    virtual ~Data() {
+    }
+    // successor/predecessor map loaded from LoadDependency
+    std::unordered_map<std::string, std::vector<std::string>> successors_;
+    std::unordered_map<std::string, std::vector<std::string>> predecessors_;
+    std::unordered_map<std::string, std::function<void(void*)>> callbacks_;
+    std::unordered_map<std::string, std::vector<std::string>> markers_;
+    std::unordered_map<std::string, std::thread::id> marked_thread_id_;
+
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    // sync points that have been passed through
+    std::unordered_set<std::string> cleared_points_;
+    // sync points that should block execution
+    std::unordered_set<std::string> blocked_points_;
+    std::atomic<bool> enabled_;
+    int num_callbacks_running_ = 0;
+
+    void
+    LoadDependency(const std::vector<SyncPointPair>& dependencies);
+    void
+    LoadDependencyAndMarkers(const std::vector<SyncPointPair>& dependencies, const std::vector<SyncPointPair>& markers);
+    bool
+    PredecessorsAllCleared(const std::string& point);
+    void
+    SetCallBack(const std::string& point, const std::function<void(void*)>& callback) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        callbacks_[point] = callback;
+    }
+
+    void
+    ClearCallBack(const std::string& point);
+    void
+    ClearAllCallBacks();
+    void
+    EnableProcessing() {
+        enabled_ = true;
+    }
+    void
+    DisableProcessing() {
+        enabled_ = false;
+        std::lock_guard<std::mutex> lock(mutex_);
+        cv_.notify_all();  // Wake up all blocked threads
+    }
+    void
+    ClearTrace() {
+        std::lock_guard<std::mutex> lock(mutex_);
+        cleared_points_.clear();
+    }
+
+    void
+    BlockAtPoint(const std::string& point) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        blocked_points_.insert(point);
+    }
+
+    void
+    UnblockPoint(const std::string& point) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        blocked_points_.erase(point);
+        cv_.notify_all();
+    }
+
+    void
+    ClearAllBlockedPoints() {
+        std::lock_guard<std::mutex> lock(mutex_);
+        blocked_points_.clear();
+        cv_.notify_all();
+    }
+
+    void
+    Reset() {
+        std::lock_guard<std::mutex> lock(mutex_);
+        successors_.clear();
+        predecessors_.clear();
+        callbacks_.clear();
+        markers_.clear();
+        marked_thread_id_.clear();
+        cleared_points_.clear();
+        blocked_points_.clear();
+        cv_.notify_all();
+    }
+    bool
+    DisabledByMarker(const std::string& point, std::thread::id thread_id) {
+        auto marked_point_iter = marked_thread_id_.find(point);
+        return marked_point_iter != marked_thread_id_.end() && thread_id != marked_point_iter->second;
+    }
+    void
+    Process(const Slice& point, void* cb_arg);
+};
+}  // namespace milvus
+#endif  // ENABLE_SYNCPOINT
---
+++ src/syncpoint/sync_point_impl.cc
@@ -0,0 +1,124 @@
+// Adapted from RocksDB's SyncPoint implementation
+// (https://github.com/facebook/rocksdb/blob/main/test_util/sync_point.h)
+#include "syncpoint/sync_point_impl.h"
+
+#ifdef ENABLE_SYNCPOINT
+namespace milvus {
+
+void
+SyncPoint::Data::LoadDependency(const std::vector<SyncPointPair>& dependencies) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    successors_.clear();
+    predecessors_.clear();
+    cleared_points_.clear();
+    for (const auto& dependency : dependencies) {
+        successors_[dependency.predecessor].push_back(dependency.successor);
+        predecessors_[dependency.successor].push_back(dependency.predecessor);
+    }
+    cv_.notify_all();
+}
+
+void
+SyncPoint::Data::LoadDependencyAndMarkers(const std::vector<SyncPointPair>& dependencies,
+                                          const std::vector<SyncPointPair>& markers) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    successors_.clear();
+    predecessors_.clear();
+    cleared_points_.clear();
+    markers_.clear();
+    marked_thread_id_.clear();
+    for (const auto& dependency : dependencies) {
+        successors_[dependency.predecessor].push_back(dependency.successor);
+        predecessors_[dependency.successor].push_back(dependency.predecessor);
+    }
+    for (const auto& marker : markers) {
+        successors_[marker.predecessor].push_back(marker.successor);
+        predecessors_[marker.successor].push_back(marker.predecessor);
+        markers_[marker.predecessor].push_back(marker.successor);
+    }
+    cv_.notify_all();
+}
+
+bool
+SyncPoint::Data::PredecessorsAllCleared(const std::string& point) {
+    for (const auto& pred : predecessors_[point]) {
+        if (cleared_points_.count(pred) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void
+SyncPoint::Data::ClearCallBack(const std::string& point) {
+    std::unique_lock<std::mutex> lock(mutex_);
+    while (num_callbacks_running_ > 0) {
+        cv_.wait(lock);
+    }
+    callbacks_.erase(point);
+}
+
+void
+SyncPoint::Data::ClearAllCallBacks() {
+    std::unique_lock<std::mutex> lock(mutex_);
+    while (num_callbacks_running_ > 0) {
+        cv_.wait(lock);
+    }
+    callbacks_.clear();
+}
+
+void
+SyncPoint::Data::Process(const Slice& point, void* cb_arg) {
+    if (!enabled_) {
+        return;
+    }
+
+    // Must convert to std::string for remaining work.  Take
+    //  heap hit.
+    std::string point_string(point.ToString());
+    std::unique_lock<std::mutex> lock(mutex_);
+    auto thread_id = std::this_thread::get_id();
+
+    auto marker_iter = markers_.find(point_string);
+    if (marker_iter != markers_.end()) {
+        for (auto& marked_point : marker_iter->second) {
+            marked_thread_id_.emplace(marked_point, thread_id);
+        }
+    }
+
+    if (DisabledByMarker(point_string, thread_id)) {
+        return;
+    }
+
+    // Check if this point should block
+    while (blocked_points_.count(point_string) > 0) {
+        cv_.wait(lock);
+        if (!enabled_) {
+            return;
+        }
+    }
+
+    while (!PredecessorsAllCleared(point_string)) {
+        cv_.wait(lock);
+        if (DisabledByMarker(point_string, thread_id)) {
+            return;
+        }
+        // Also check if blocking was cleared
+        if (!enabled_) {
+            return;
+        }
+    }
+
+    auto callback_pair = callbacks_.find(point_string);
+    if (callback_pair != callbacks_.end()) {
+        num_callbacks_running_++;
+        mutex_.unlock();
+        callback_pair->second(cb_arg);
+        mutex_.lock();
+        num_callbacks_running_--;
+    }
+    cleared_points_.insert(point_string);
+    cv_.notify_all();
+}
+}  // namespace milvus
+#endif  // ENABLE_SYNCPOINT
